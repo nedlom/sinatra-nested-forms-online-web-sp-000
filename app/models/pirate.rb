2764272cff6a9958666c0915524e048afca77966
imperative-scientist-4600 <nelom3@gmail.com>
2021-04-20 19:00:29 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  
+  attr_accessor :name, :weight, :height
+  
+  @@all = []
+  
+  def initialize(params)
+    @name = params["name"]
+    @weight = params["weight"]
+    @height = params["height"]
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
